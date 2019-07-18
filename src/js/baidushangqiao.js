c3ed616f9b7c8e659d6a9b054a2d242a3a482006
shengli <363912809@qq.com>
2019-07-18 10:15:08 +0800

    shangqiao

--- src/js/baidushangqiao.js
+++ src/js/baidushangqiao.js
@@ -5,3 +5,13 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
+
+$(function(){
+    // 点击按钮时判断 百度商桥代码中的“我要咨询”按钮的元素是否存在，存在的话就执行一次点击事件
+    $(".shangqiao").click(function(event) {
+        if ($('#nb_invite_ok').length > 0) {
+            $('#nb_invite_ok').click();
+        }
+    });
+});
