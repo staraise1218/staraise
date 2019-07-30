e60cc93ce39e6fbf4352bb75176311e801baec8a
guofeng-mi <guofeng_mi@sina.com>
2019-07-30 13:36:38 +0800

    akjbsfkz

---
+++ m/src/js/index.js
@@ -0,0 +1,33 @@
+/**
+ * 顶部轮播图
+ */
+var mySwiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    a11y: {
+        prevSlideMessage: 'Previous slide',
+        nextSlideMessage: 'Next slide',
+        firstSlideMessage: 'This is the first slide',
+        lastSlideMessage: 'This is the last slide',
+        paginationBulletMessage: 'Go to slide {{index}}',
+        notificationClass: 'swiper-notification',
+    },
+    speed: 3000,
+    grabCursor : true,
+    autoplay: {
+        delay: 800,
+        disableOnInteraction: false,
+    },
+    effect : 'fade',
+})
+
+
+$('.nav-top .right img').click(function () {
+    $('.nav_ul').toggle();
+})
