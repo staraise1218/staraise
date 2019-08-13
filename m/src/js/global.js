f727742722655d743b94919bd90c814afbc9c948
guofeng-mi <guofeng_mi@sina.com>
2019-08-13 15:43:09 +0800

    aslfnknjzklllz

--- m/src/js/global.js
+++ m/src/js/global.js
@@ -2,21 +2,12 @@
 var _hmt = _hmt || [];
 
 
-
-function sqInit () {
-	var hm = document.createElement("script");
-	hm.src = "https://hm.baidu.com/hm.js?1be9b52e79a52be423089ebb6868a7f5";
-	var s = document.getElementsByTagName("script")[0]; 
-	s.parentNode.insertBefore(hm, s);
-  }
-
-
-// (function() {
-//   var hm = document.createElement("script");
-//   hm.src = "https://hm.baidu.com/hm.js?1be9b52e79a52be423089ebb6868a7f5";
-//   var s = document.getElementsByTagName("script")[0]; 
-//   s.parentNode.insertBefore(hm, s);
-// })();
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1be9b52e79a52be423089ebb6868a7f5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
 
 
 $(function(){
@@ -29,9 +20,7 @@ $(function(){
 });
 
 
-$('.rr .box').click(function () {
-	sqInit();
-})
+
 
 
 
