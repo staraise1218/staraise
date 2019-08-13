1f4a40971c887659b770ce14427c24bed3ae405a
guofeng-mi <guofeng_mi@sina.com>
2019-08-13 15:41:42 +0800

    asdflnzjnkq

--- m/src/js/global.js
+++ m/src/js/global.js
@@ -1,11 +1,22 @@
 // 百度商桥
 var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?1be9b52e79a52be423089ebb6868a7f5";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
+
+
+
+function sqInit () {
+	var hm = document.createElement("script");
+	hm.src = "https://hm.baidu.com/hm.js?1be9b52e79a52be423089ebb6868a7f5";
+	var s = document.getElementsByTagName("script")[0]; 
+	s.parentNode.insertBefore(hm, s);
+  }
+
+
+// (function() {
+//   var hm = document.createElement("script");
+//   hm.src = "https://hm.baidu.com/hm.js?1be9b52e79a52be423089ebb6868a7f5";
+//   var s = document.getElementsByTagName("script")[0]; 
+//   s.parentNode.insertBefore(hm, s);
+// })();
 
 
 $(function(){
@@ -18,7 +29,9 @@ $(function(){
 });
 
 
-
+$('.rr .box').click(function () {
+	sqInit();
+})
 
 
 
