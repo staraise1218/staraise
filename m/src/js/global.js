dc7f9dd56562aebcdbc11b0f6a8f65cc168ddd07
guofeng-mi <guofeng_mi@sina.com>
2019-08-13 15:25:44 +0800

    alsljkfniknznjkaa

--- m/src/js/global.js
+++ m/src/js/global.js
@@ -1,3 +1,31 @@
+// 百度商桥
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1be9b52e79a52be423089ebb6868a7f5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+$(function(){
+    // 点击按钮时判断 百度商桥代码中的“我要咨询”按钮的元素是否存在，存在的话就执行一次点击事件
+    $(".shangqiao").click(function(event) {
+        if ($('#nb_invite_ok').length > 0) {
+            $('#nb_invite_ok').click();
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
 //designWidth:设计稿的实际宽度值，需要根据实际设置
 //maxWidth:制作稿的最大宽度值，需要根据实际设置
 //这段js的最后面有两个参数记得要设置，一个为设计稿实际宽度，一个为制作稿最大宽度，例如设计稿为750，最大宽度为750，则为(750,750)
