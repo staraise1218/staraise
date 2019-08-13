2ebca93b1b20e3792ed8464c7cffa3a3a272449b
guofeng-mi <guofeng_mi@sina.com>
2019-08-13 15:29:15 +0800

    alsjndfikjnbkz

--- m/src/js/global.js
+++ m/src/js/global.js
@@ -10,7 +10,7 @@ var _hmt = _hmt || [];
 
 $(function(){
     // 点击按钮时判断 百度商桥代码中的“我要咨询”按钮的元素是否存在，存在的话就执行一次点击事件
-    $(".shangqiao").click(function(event) {
+	$('.rr .box').click(function(event) {
         if ($('#nb_invite_ok').length > 0) {
             $('#nb_invite_ok').click();
         }
