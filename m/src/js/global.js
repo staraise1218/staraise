41c34943f46e0bb1b0e42be3d2927b0eeaeaa3d6
guofeng-mi <guofeng_mi@sina.com>
2019-08-13 15:31:34 +0800

    asdlfknkjnkz

--- m/src/js/global.js
+++ m/src/js/global.js
@@ -8,11 +8,13 @@ var _hmt = _hmt || [];
 })();
 
 
-// 点击按钮时判断 百度商桥代码中的“我要咨询”按钮的元素是否存在，存在的话就执行一次点击事件
-$('.rr .box').click(function(event) {
-	if ($('#nb_invite_ok').length > 0) {
-		$('#nb_invite_ok').click();
-	}
+$(function(){
+    // 点击按钮时判断 百度商桥代码中的“我要咨询”按钮的元素是否存在，存在的话就执行一次点击事件
+    $(".shangqiao").click(function(event) {
+        if ($('#nb_invite_ok').length > 0) {
+            $('#nb_invite_ok').click();
+        }
+    });
 });
 
 
